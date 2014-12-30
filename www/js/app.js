f3868c26fdede2007666ba3408c70c481a1e64f1
stlrenaj <stlrenaj@stl-6232mac.stl.leeent.net>
2014-12-29 18:32:04 -0600

    template fixes

--- www/js/app.js
+++ www/js/app.js
@@ -15,25 +15,39 @@ jQuery( document ).ready(function( $ ) {
 	window.jsprettify.run();
 
 	$('.topic h2').click( function(){
-		$(this).parent().toggleClass('active');
 		$theSwiper = $(this).parent().children('.swiper-container');
+		// this element is already active, so let's deactivate it
 		if ( $(this).parent().hasClass('active') ) {
+			$theSwiper.swiper().destroy();
+			$theSwiper.children('.arrow-left').off();
+			$theSwiper.children('.arrow-right').off();
+			$theSwiper.children('.pagination').empty();
+			$('div.active').removeClass('active');
+		}
+		// this element is NOT active, so let's activate it
+		else {
+			// first, deactivate any other currently-active element
+			$alreadyActive = $('div.active').children('.swiper-container');
+			if ( $alreadyActive.length > 0 ) {
+				$alreadyActive.swiper().destroy();
+				$alreadyActive.children('.arrow-left').off();
+				$alreadyActive.children('.arrow-right').off();
+				$alreadyActive.children('.pagination').empty();
+				$alreadyActive.parent().removeClass('active');
+			}
+			// now activate this element
+			$(this).parent().addClass('active');
+			// create swiper object
 			var mySwiper = $theSwiper.swiper({
 				pagination: $theSwiper.find('.pagination')[0],
 				createPagination: true,
 				paginationClickable: true
 			});
+			// add nav arrows
 			$theSwiper.children('.arrow-left').click(function(e){ mySwiper.swipePrev() });
 			$theSwiper.children('.arrow-right').click(function(e){ mySwiper.swipeNext() });
-			// console.log( $theSwiper );
-
-		}
-		else {
-			console.log('INACTIVE!');
-			$theSwiper.swiper().destroy();
-			$theSwiper.children('.arrow-left').off();
-			$theSwiper.children('.arrow-right').off();
-			// console.log( $theSwiper );
+			// scroll to the element we just clicked, in case we got knocked out of whack		
+			$( document ).scrollTo( $(this), 800 );
 		}
 	});
 });
